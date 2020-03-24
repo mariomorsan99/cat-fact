9aee6f1b3dc1ab3a3c7a8fd121ad052d7e3fde28
Mario alberto sanchez <mario_alberto_sanchez@hotmail.com>
2020-03-24 00:31:06 -0600

    cambios visuales en la plantilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare function init_plugin();
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'appAptude';
+
+  constructor() { }
+
+  ngOnInit() {
+    init_plugin();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { HeaderComponent } from './shared/header/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar/sidebar.component';
 import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
 import { FactService } from './providers/fact.service';
-import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +21,6 @@ import { LoginComponent } from './components/login/login.component';
     HeaderComponent,
     SidebarComponent,
     BreadcrumsComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
